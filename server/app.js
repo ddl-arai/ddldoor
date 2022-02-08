137323dae39c33452785318d97746ff6647bb4f4
ddl-arai <arai.koki@ddl.co.jp>
2022-02-08 18:10:26 +0900

    c

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      secure: true
+      secure: false // https => true
     },
     proxy: true
 }));
