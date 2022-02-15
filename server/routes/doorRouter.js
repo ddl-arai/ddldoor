27c7322b1b62609889787db6ef43775804729efb
ddl-arai <arai.koki@ddl.co.jp>
2022-02-15 15:06:20 +0900

    s

--- server/routes/doorRouter.js
+++ server/routes/doorRouter.js
@@ -17,92 +17,95 @@ doorRouter.get('/' , async (req, res, next) => {
 
     switch (req.query.request) {
         case 'stamp':
-            try {
-                /* First save log */
-                await Log.create({
-                    sec: req.query.sec,
-                    idm: req.query.idm,
-                    devid: req.query.devid
-                }).exec();
-
+            /* First save log */
+            Log.create({
+                sec: req.query.sec,
+                idm: req.query.idm,
+                devid: req.query.devid
+            }, error => {
+                if(error) next(error);
+                
                 /* Manage status */
-                let card = await Card.findOne({idm: req.query.idm}).exec();
-                if(!card){
-                    res.json({
-                        result: 2,
-                        message: 'Not registered idm'
-                    });
-                    break;
-                }
-                let device = await Device.findOne({id: req.query.devid}).exec();
-                if(!device){
-                    res.json({
-                        result: 2,
-                        message: 'Not registered device'
-                    });
-                    break;
-                }
-                let member = await Member.findOne({id: card.id}).exec();
-                if(member.initial){
-                    member.initial = false;
-                    switch(device.func){
-                        case 'enter':
-                            member.attendance = true;
-                            break;
-                        case 'exit':
-                            member.attendance = false;
-                            break;
-                        default:
-                            break;
+                try {
+                    let card = await Card.findOne({idm: req.query.idm}).exec();
+                    if(!card){
+                        res.json({
+                            result: 2,
+                            message: 'Not registered idm'
+                        });
+                        break;
                     }
-                    await member.save();
-                    res.json({
-                        result: 0,
-                        message: 'Success'
-                    });
-                }
-                else{
-                    switch(device.func){
-                        case 'enter':
-                            if(member.attendance){
-                                res.json({
-                                    result: 1,
-                                    message: 'APB Error'
-                                });
-                            }
-                            else{
+                    let device = await Device.findOne({id: req.query.devid}).exec();
+                    if(!device){
+                        res.json({
+                            result: 2,
+                            message: 'Not registered device'
+                        });
+                        break;
+                    }
+                    let member = await Member.findOne({id: card.id}).exec();
+                    if(member.initial){
+                        member.initial = false;
+                        switch(device.func){
+                            case 'enter':
                                 member.attendance = true;
-                                await member.save();
-                                res.json({
-                                    result: 0,
-                                    message: 'Success'
-                                });
-                            }
-                            break;
-                        case 'exit':
-                            if(!member.attendance){
-                                res.json({
-                                    result: 1,
-                                    message: 'APB Error'
-                                });
-                            }
-                            else{
+                                break;
+                            case 'exit':
                                 member.attendance = false;
-                                await member.save();
-                                res.json({
-                                    result: 0,
-                                    message: 'Success'
-                                });
-                            }
-                            break;
-                        default:
-                            break;
+                                break;
+                            default:
+                                break;
+                        }
+                        await member.save();
+                        res.json({
+                            result: 0,
+                            message: 'Success'
+                        });
+                    }
+                    else{
+                        switch(device.func){
+                            case 'enter':
+                                if(member.attendance){
+                                    res.json({
+                                        result: 1,
+                                        message: 'APB Error'
+                                    });
+                                }
+                                else{
+                                    member.attendance = true;
+                                    await member.save();
+                                    res.json({
+                                        result: 0,
+                                        message: 'Success'
+                                    });
+                                }
+                                break;
+                            case 'exit':
+                                if(!member.attendance){
+                                    res.json({
+                                        result: 1,
+                                        message: 'APB Error'
+                                    });
+                                }
+                                else{
+                                    member.attendance = false;
+                                    await member.save();
+                                    res.json({
+                                        result: 0,
+                                        message: 'Success'
+                                    });
+                                }
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
-            }
-            catch(error){
-                next(error);
-            } 
+                catch(error){
+                    next(error);
+                } 
+
+            });
             break;
         case 'open':
             break;
