5d2c7f698887ef114e3464af9fa23aed48ab8f46
ddl-arai <arai.koki@ddl.co.jp>
2022-02-17 19:15:34 +0900

    schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ let mongoose = require("mongoose"),
  userSchema = mongoose.Schema({
     email: String,
     password: String,
+    pw_reset_token: String,
+    pw_reset_token_expire: Date
   });
 
 module.exports = mongoose.model("User", userSchema, 'user');
