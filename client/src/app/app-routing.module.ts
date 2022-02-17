fb29a2979b79da59dd4f9a5f7c59ac6d301e69ce
ddl-arai <arai.koki@ddl.co.jp>
2022-02-17 16:11:03 +0900

    c

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -26,6 +26,7 @@ const routes: Routes = [
       { path: 'account', component: AccountComponent }
     ]
   },
+  //{ path: 'account', component: AccountComponent },
   { path: 'nfc', component: NfcComponent },
   { path: "**", redirectTo: "home" }
 ];
