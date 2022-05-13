3314773d105535464fc4acd258a147893ef6da7b
ddl-arai <arai.koki@ddl.co.jp>
2022-05-13 18:27:28 +0900

    Fix check request

--- server/routes/doorRouter.js
+++ server/routes/doorRouter.js
@@ -643,7 +643,7 @@ doorRouter.get('/', async (req, res, next) => {
           $or: [
             { $and: [{ devid: device.id, result: LOG_OK, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.id, result: LOG_OK, sec: { $lte: Number(req.query.sec) } }] },
             { $and: [{ devid: device.id, result: LOG_GUEST, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.id, result: LOG_GUEST, sec: { $lte: Number(req.query.sec) } }] },
-            { $and: [{ devid: device.id, result: LOG_TMP_OPEN, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.id, result: LOG_GUEST, sec: { $lte: Number(req.query.sec) } }] }
+            { $and: [{ devid: device.id, result: LOG_TMP_OPEN, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.id, result: LOG_TMP_OPEN, sec: { $lte: Number(req.query.sec) } }] }
           ]
         }).exec();
         console.log('********** Check Log **********');
@@ -652,7 +652,7 @@ doorRouter.get('/', async (req, res, next) => {
           $or: [
             { $and: [{ devid: device.partnerId, result: LOG_OK, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.partnerId, result: LOG_OK, sec: { $lte: Number(req.query.sec) } }] },
             { $and: [{ devid: device.partnerId, result: LOG_GUEST, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.partnerId, result: LOG_GUEST, sec: { $lte: Number(req.query.sec) } }] },
-            { $and: [{ devid: device.partnerId, result: LOG_TMP_OPEN, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.partnerId, result: LOG_GUEST, sec: { $lte: Number(req.query.sec) } }] },
+            { $and: [{ devid: device.partnerId, result: LOG_TMP_OPEN, sec: { $gt: Number(req.query.sec) - 5 } }, { devid: device.partnerId, result: LOG_TMP_OPEN, sec: { $lte: Number(req.query.sec) } }] },
           ]
         }).exec();
         console.log(`partner_log: ${JSON.stringify(partner_log)}`);
